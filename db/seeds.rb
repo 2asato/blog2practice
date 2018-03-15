1b7007c3b069d76007cf4ab17da9fb1a423628c7
aaronsato <2asato@gmail.com>
2018-03-15 10:49:34 -0700

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{ name: "Cas" }, { name: "Teyo" }])
+
+Post.create(title: "Roudy and Angle", author: "Teyo", content: "Roudy and Angle teaming up at Wrestlemania", user_id: 4)
+
+Post.create(title: "The Beast", author: "Teyo", content: "Stroman going solo in Tag Team Championship match", user_id: 4)
+
+Post.create(title: "Cena wants in", author: "Teyo", content: "John Cena challenges Undertaker to match at Wrestlemania", user_id: 4)
